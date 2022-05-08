8de58e2b385e7c4fa6c7ee0d941effac69c5e179
Rajiv Blancke <blancke.rajiv@gmail.com>
2022-05-08 17:33:40 +0200

    rename

---
+++ src/com/test/java17/C_PlayWIthStreams.java
@@ -0,0 +1,13 @@
+package com.test.java17;
+
+import java.util.List;
+
+public class C_PlayWIthStreams {
+    public static void main(String[] args) {
+        var listOf = List.of("one", "two", "three", "four", "five");
+        var count = listOf.stream()
+                .count();
+
+        System.out.println("Count: " + count);
+    }
+}

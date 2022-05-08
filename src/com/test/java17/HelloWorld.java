6e837b14aaabdac9d74b31a5e636cf6f6774c25d
Rajiv Blancke <blancke.rajiv@gmail.com>
2022-05-08 17:08:57 +0200

    add PlayWithString

--- src/com/test/java17/HelloWorld.java
+++ src/com/test/java17/HelloWorld.java
@@ -1,3 +1,5 @@
+package com.test.java17;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
---
+++ src/com/test/java17/PlayWithStrings.java
@@ -0,0 +1,4 @@
+package com.test.java17;
+
+public class PlayWithStrings {
+}

80cab2d0295b8e4dd926eeab8c07415e661909cf
Rajiv Blancke <blancke.rajiv@gmail.com>
2022-05-08 17:27:27 +0200

    add PlayWithString

--- src/com/test/java17/PlayWithStrings.java
+++ src/com/test/java17/PlayWithStrings.java
@@ -1,4 +1,9 @@
 package com.test.java17;
 
 public class PlayWithStrings {
+    public static void main(String[] args) {
+        String numbers = "one two three four five";
+
+        System.out.println("numbers = " + numbers);
+    }
 }

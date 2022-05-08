9b832b2a50954e5f969cfe9a96f332594eb3459a
Rajiv Blancke <blancke.rajiv@gmail.com>
2022-05-08 21:42:22 +0200

    add PlayWithSwitch

--- src/com/test/java17/H_PlayWithInstanceOf.java
+++ src/com/test/java17/H_PlayWithInstanceOf.java
@@ -4,16 +4,16 @@ import com.test.java17.model.Rectangle;
 import com.test.java17.model.Shape;
 import com.test.java17.model.Square;
 
-public class H_InstanceOf {
+public class H_PlayWithInstanceOf {
     public static void main(String[] args) {
 
     }
 
     public int surface(Shape shape) {
-        if (shape instanceof Square){
-            return ((Square) shape).edge() * ((Square) shape).edge();
-        } else if (shape instanceof Rectangle) {
-            return ((Rectangle) shape).height() * ((Rectangle) shape).width();
+        if (shape instanceof Square square){
+            return square.edge() * square.edge();
+        } else if (shape instanceof Rectangle rect) {
+            return rect.height() * rect.width();
         }
         //circle missing
         return 0;
---
+++ src/com/test/java17/J_PlayWithSwitch.java
@@ -0,0 +1,22 @@
+package com.test.java17;
+
+import com.test.java17.model.Rectangle;
+import com.test.java17.model.Shape;
+import com.test.java17.model.Square;
+
+public class J_PlayWithSwitch {
+    public static void main(String[] args) {
+
+    }
+
+
+    public int surface(Shape shape) {
+        //return switch (shape) {
+        //    case Rectangle rectangle:
+        //        yield rectangle.height() * rectangle.width();
+        //    case Square square:
+        //        yield square.edge() * square.edge();
+        //};
+        return 0;
+    }
+}

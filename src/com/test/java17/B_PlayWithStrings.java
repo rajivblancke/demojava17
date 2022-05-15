4a57c6285477174ba743bc4350e2adeeebce1833
rajiv.blancke <blancke.rajiv@gmail.com>
2022-05-15 16:22:40 +0200

    Extra sample code PlayWithStrings

--- src/com/test/java17/B_PlayWithStrings.java
+++ src/com/test/java17/B_PlayWithStrings.java
@@ -5,5 +5,26 @@ public class B_PlayWithStrings {
         String numbers = "one two three four five";
 
         System.out.println("numbers = " + numbers);
+
+        //textblock
+        /*String numbersTextBlock = """
+             one
+             "two"
+             'three'
+             four
+             five
+          """;
+
+        System.out.println("numbers = " + numbersTextBlock);*/
+
+        //textblock trailing spaces
+        /*String numberTrailingSpaces = """
+             one     \s
+             "two"   \s
+             'three' \s
+             four    \s
+             five    \s
+          """;
+        numberTrailingSpaces.lines().map(s -> "|" + s + "|").forEach(System.out::println);*/
     }
 }

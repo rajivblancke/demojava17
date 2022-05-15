43bebfe15b29f4bad9e47e09a8a810c0c7a6bdba
rajiv.blancke <blancke.rajiv@gmail.com>
2022-05-15 17:24:56 +0200

    Extra sample code PlayWIthRecordsAndIO

--- src/com/test/java17/E_PlayWIthRecordsAndIO.java
+++ src/com/test/java17/E_PlayWIthRecordsAndIO.java
@@ -7,9 +7,9 @@ import java.nio.file.Path;
 public class E_PlayWIthRecordsAndIO {
     static record RangeRecord(int begin, int end) implements Serializable {
         RangeRecord {
-            if (begin > end) {
+/*            if (begin > end) {
                 throw new IllegalArgumentException("End must be greater than begin");
-            }
+            }*/
         }
     }
 
@@ -18,9 +18,9 @@ public class E_PlayWIthRecordsAndIO {
         private final int end;
 
         RangeClass(int begin, int end) {
-            if (begin > end) {
+/*            if (begin > end) {
                 throw new IllegalArgumentException("Begin must be smaller than end");
-            }
+            }*/
             this.begin = begin;
             this.end = end;
             System.out.println("Building RangeClass");
@@ -36,17 +36,34 @@ public class E_PlayWIthRecordsAndIO {
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        //var range = new RangeRecord(10, 0);
-        //System.out.println("range = " + range);
+        //write corrupt rangeclass
+        var range = new RangeClass(10, 0);
+        System.out.println("range = " + range);
 
-        //var fos = Files.newOutputStream(Path.of("files/range_record_10_0.dat"));
-        //var oos = new ObjectOutputStream(fos);
-        //oos.writeObject(range);
+        var fos = Files.newOutputStream(Path.of("files/range_class_10_0.dat"));
+        var oos = new ObjectOutputStream(fos);
+        oos.writeObject(range);
 
-        var fis = Files.newInputStream(Path.of("files/range_record_10_0.dat"));
+        //read corrupt range instance
+/*        var fis = Files.newInputStream(Path.of("files/range_class_10_0.dat"));
         var ois = new ObjectInputStream(fis);
         System.out.println("Reading range");
         var readRange = ois.readObject();
-        System.out.println("readRange = " + readRange);
+        System.out.println("readRange = " + readRange);*/
+
+        //write corrupt rangerecord
+/*        var range = new RangeRecord(10, 0);
+        System.out.println("range = " + range);
+
+        var fos = Files.newOutputStream(Path.of("files/range_record_10_0.dat"));
+        var oos = new ObjectOutputStream(fos);
+        oos.writeObject(range);*/
+
+        //read corrupt rangerecord
+/*        var fis = Files.newInputStream(Path.of("files/range_record_10_0.dat"));
+        var ois = new ObjectInputStream(fis);
+        System.out.println("Reading range");
+        var readRange = ois.readObject();
+        System.out.println("readRange = " + readRange);*/
     }
 }
